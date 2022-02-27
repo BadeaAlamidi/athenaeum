e46302c7cf2d93400c1bdaeb87ae802e9d49ea81
Badea Alamidi <badeaalamidi@gmail.com>
2022-02-27 17:33:05 -0600

    Create book component

---
+++ client/src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+	<input type="text" placeholder="search here">
+</nav>
+<slot></slot>
---
+++ client/src/routes/_bookComponent.svelte
@@ -0,0 +1,13 @@
+<div class='wrapper' style=''>
+    <div>
+        <slot name='image'>book image</slot>
+    </div>
+    <slot name='title'>book title</slot>
+</div>
+
+<style>
+    .wrapper { 
+        border-style:solid;
+        border-color:red;
+    }
+</style>
