3837f8137827840bb58b90fd01e10fc9b5ca23f7
Anthony Chavez <anthony@anthonychavez.xyz>
2022-03-20 15:16:56 -0500

    Place the books in a grid, and try to make images the same size

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -9,13 +9,29 @@
      console.log(books);
  })
 </script>
+<style>
+ .grid-container {
+     display: grid;
+     grid-template-columns: auto auto auto;
+     grid-template-rows: auto;
+     grid-gap: 0.5rem;
+ }
+ .grid-items img {
+     width: 200px;
+     height: 300px;
+     object-fit: cover;
+ }
+</style>
 <h1>Welcome to Athenaeum</h1>
-
+<div class="grid-container">
 {#each books as book}
+  <div class="grid-items">
     <BookComponent>
         <span slot="title">{book.title}</span>
         <span slot="image"><img src="{book.thumbnailUrl}"></span>
     </BookComponent>
+  </div>
 {:else}
     <p>Loading Books...</p>
 {/each}
+</div>
