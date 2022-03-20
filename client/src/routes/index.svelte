026e3fd370b584bab7ebbbf85a03a31022507533
Anthony Chavez <anthony@anthonychavez.xyz>
2022-03-20 00:13:12 -0500

    Display books from database on a basic home page
    fetch the books from the backend server, and display  them on the home
    page. It's very basic for now, but it'll be worked on in the future.

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,8 +1,22 @@
 <script>
-    import BookComponent from "./_bookComponent.svelte";
+ import BookComponent from "./_bookComponent.svelte";
+ import { onMount } from 'svelte';
+
+ let books = [];
+ onMount(async () => {
+     const res = await fetch("http://localhost:5000/books");
+     books = await res.json();
+     console.log(books);
+ })
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<BookComponent>
-    <img slot='image' src="http://books.google.com/books/content?id=5iwwAQAAMAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="book cover">
-</BookComponent>
+{#each books as book}
+    <p>{book.title}</p>
+    <BookComponent>
+        <span slot="title">{book.title}</span>
+        <span slot="image"><img src="{book.thumbnailUrl}"></span>
+    </BookComponent>
+{:else}
+    <p>Loading Books...</p>
+{/each}
