fab7ff9d8ac646862eaddf581d8866dcd87f069f
Badea Alamidi <badeaalamidi@gmail.com>
2022-03-19 19:36:51 -0500

    create book component in front-end

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -3,4 +3,6 @@
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<BookComponent></BookComponent>
+<BookComponent>
+    <img slot='image' src="http://books.google.com/books/content?id=5iwwAQAAMAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="book cover">
+</BookComponent>
