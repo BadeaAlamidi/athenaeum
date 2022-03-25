7d45aebbc5dbd94ced593a5e5fda22686f7fff7b
Badea Alamidi <badeaalamidi@gmail.com>
2022-03-25 01:59:31 -0500

    Create migration file for authors
    The foreign key constraint had to be established manually by calling
    the async function after declaring the non-foreign columns.

---
+++ server/migrations/20220320034958-create-author.cjs
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Authors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    await queryInterface.addColumn(
+      'Authors',
+      'bookId',
+      {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Books',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Authors');
+    await queryInterface.removeColumn(
+      'Authors',
+      'bookId'
+    );
+  }
+};
