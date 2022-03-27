7644ed846699010798984a8e91e58835b8b80886
Badea Alamidi <badeaalamidi@gmail.com>
2022-03-26 23:47:04 -0500

    change new models to ES modules

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ let port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  console.log("New request!");
+  console.log("New request");
   res.send("Hello, world!");
 });
 
