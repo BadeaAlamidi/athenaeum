3f3f080b4a7738a515e6dee1a8b7ce1c85ad952b
Badea Alamidi <badeaalamidi@gmail.com>
2022-04-13 17:11:12 -0500

    Add logic for book controller for search queries
    The book controller can now query the database for matches that
    are similar to the provided query search string from the client's search
    component

--- server/controllers/Books.js
+++ server/controllers/Books.js
@@ -1,12 +1,45 @@
 // this file was named with the .cjs extension in order to allow the usage of requrie. this can be circumvented by
 // not using module.exports in the model file referenced below.
 import models from '../models/index.js';
+import {Op} from 'sequelize';
 
 const getBooks = async (req,res) =>{
     let books;
     if (req.query){
+        let whereClause;
+        switch (true){
+            case /^isbn10:/.test(req.query.searchString) :
+                whereClause = {
+                    isbn10:{
+                        [Op.eq] : req.query.searchString.slice(7)
+                    }
+                }
+            break;
+            case /^isbn13:/.test(req.query.searchString) : 
+                whereClause = {
+                    isbn13:{
+                        [Op.eq] : req.query.searchString.slice(7)
+                    }
+                }
+            break;
+            // case req.query.searchString === '':
+            //     whereClause = {
+            //         title:{
+            //             [Op.like] : '%'
+            //         }
+            //     }
+            // break;
+            default:
+                whereClause = {
+                    title:{
+                        [Op.like] : '%' + req.query.searchString + '%'
+                    }
+                }
+            break;
+        }
         // api/books?
         if (req.query.order){
+            console.log(req.query);
             // api/books?order
             const columns = ['title','publishDate','rating','pageCount','id'];
             if (!columns.includes(req.query.order)){
@@ -21,11 +54,16 @@ const getBooks = async (req,res) =>{
             }
             books = await models.Book.findAll({
                 raw:true,
-                order: [[req.query.order,direction]]
+                order: [[req.query.order,direction]],
+                where: whereClause,
+                
             });
         } else {
-            // api/books? no  order
-            books = await models.Book.findAll({raw:true});
+            // api/books? no  order.
+            books = await models.Book.findAll({
+                raw:true,
+                where: whereClause
+            });
         }
     } else {
         // api/books
