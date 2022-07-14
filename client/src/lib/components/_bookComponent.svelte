3412b631577a81bcd0c84ef0164b00334f3740df
Badea Alamidi <badeaalamidi@gmail.com>
2022-07-14 18:12:17 -0500

    Add event handler on book components to navigate

--- client/src/lib/components/_bookComponent.svelte
+++ client/src/lib/components/_bookComponent.svelte
@@ -1,7 +1,13 @@
 <script>
+    import {page} from '$app/stores'
+
     export let id;
+
+    function navToBook(){
+        window.location.href = window.location.protocol + '//' + $page.url.host + '/book/' + id;
+    }
 </script>
-<div class='wrapper' style='' title={id}>
+<div class='wrapper' style='' title={id} on:click={navToBook}>
     <div>
         <slot name='image'>no image</slot>
     </div>
@@ -10,6 +16,7 @@
 
 <style>
     .wrapper {
+        cursor:pointer;
         border-style:solid;
         border-color:red;
     }
